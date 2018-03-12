a7f281fe4f12cc447d1e815b7d4cc51c6b46f6f2
lichen <xy1ona@163.com>
2018-03-12 11:05:57 +0800

    demo1 init

---
+++ my-project/src/b.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>{{ hello }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        hello: '我是componentB'
+      }
+    },
+  }
+</script>
