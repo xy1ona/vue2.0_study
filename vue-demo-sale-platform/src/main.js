984d22d38b807b0f61b9ce708c134efa1ddbe76a
lichen <xy1ona@163.com>
2018-03-22 14:50:26 +0800

    [element-ui]

--- vue-demo-sale-platform/src/main.js
+++ vue-demo-sale-platform/src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import LayOut from './components/layout'
 import router from './router'
 import VueResource from 'vue-resource'
 import store from './store/vuex'
 
+Vue.use(ElementUI, { size: 'small' })
 Vue.use(VueResource)
 Vue.config.productionTip = false
 
